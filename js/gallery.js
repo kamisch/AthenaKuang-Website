50e5b3047c9f58575ae818cfdc6fdf2716cd8ac0
kamisch <chengtwv2@gmail.com>
2019-12-05 15:58:42 -0500

    final push

--- js/gallery.js
+++ js/gallery.js
@@ -3,9 +3,9 @@ const renderFrame = (obj) => {
     <div class = "frame">
         <div class = "window">
             <h1>${obj.title}</h1>
-            <img src = ${obj.img}/>
+            <img src = "${obj.img}"/>
             <p>${obj.description}</p>
-            <p>${obj.other}</p>
+            <p>Check out <a href = "${obj.other}">this link</a> to find more about this project </p>
         </div>
         <button id = "cancel"><i class="fas fa-window-close"></i></button>
     </div>`;
@@ -16,10 +16,10 @@ const handleFrameClose = (event) => {
 }
 const handleFrameClick = (event) => {
     const context = $(event.target);
-    const title = context.children("h4").text();
-    const description = context.children("p").text();
-    const img = context.children("img").attr('src');;
-    const other = context.children(".other").text();
+    const title = context.parent().children("h4").text();
+    const description = context.parent().children("h6").data("p");
+    const img = context.parent().children("h6").data("img");
+    const other = context.parent().children("h6").data("other");
     const obj = {
         title:title,
         description:description,
@@ -30,6 +30,6 @@ const handleFrameClick = (event) => {
 }
 
 $(function() {
-    $("#main").on("click",".grid-item",handleFrameClick)
+    $("#main").on("click","h6",handleFrameClick)
     $("#main").on("click","#cancel",handleFrameClose)
 });
